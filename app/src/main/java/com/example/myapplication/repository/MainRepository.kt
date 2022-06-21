a9f17b42ac8016e0153db8f30b49dc1d17ed5f03
Anatolii Sviderskyi <anatolii.sviderskyi@virtual-solution.com>
2022-06-21 15:54:25 +0200

    Add shop datasource to the projeck

--- app/src/main/java/com/example/myapplication/repository/MainRepository.kt
+++ app/src/main/java/com/example/myapplication/repository/MainRepository.kt
@@ -1,32 +1,9 @@
 package com.example.myapplication.repository
 
-import android.os.SystemClock
-import com.example.myapplication.view.main.ShopInfo
+import com.example.myapplication.datasource.ShopDataSource
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class MainRepository {
-    val shopList = flow<List<ShopInfo>?> {
-
-        val shops = mutableListOf(
-            ShopInfo("ALDI", "Tulpenweg 1, 15834, Rangsdorf", 4,"Distance - 3 km"),
-            ShopInfo("LIDL", "Tulpenweg 2, 15834, Rangsdorf", 4, "Distance 2 km"),
-            ShopInfo("EDEKA", "Tulpenweg 3, 15834, Rangsdorf", 5, "Distance 5 km"),
-            ShopInfo("NETTO", "Tulpenweg 4, 15834, Rangsdorf", 3, "Distance 1 km"),
-            ShopInfo("ALDI", "Tulpenweg 7, 15834, Rangsdorf", 4, "Distance 2 km"),
-            ShopInfo("LIDL", "Tulpenweg 8, 15834, Rangsdorf", 4, "Distance 3 km"),
-            ShopInfo("EDEKA", "Tulpenweg 9, 15834, Rangsdorf", 3,"Distance 7 km"),
-            ShopInfo("NETTO", "Tulpenweg 10, 15834, Rangsdorf", 3, "Distance 5 km"))
-        while (true) {
-            val millis = System.currentTimeMillis()
-            if (millis % 2L == 0L) {
-                emit(null)
-            } else {
-                emit(shops)
-            }
-            SystemClock.sleep(2000)
-        }
-
-    }.flowOn(Dispatchers.IO)
+class MainRepository(shopDataSource: ShopDataSource = ShopDataSource()) {
+    val shopList = shopDataSource.shopList.flowOn(Dispatchers.IO)
 }
