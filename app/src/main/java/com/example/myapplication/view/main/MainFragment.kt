255d4323552e462a9ffe8e311128b0aabab2923b
Anatolii Sviderskyi <anatolii.sviderskyi@virtual-solution.com>
2022-07-16 00:06:49 +0200

    Add a search bar and a listener

--- app/src/main/java/com/example/myapplication/view/main/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/view/main/MainFragment.kt
@@ -2,6 +2,9 @@ package com.example.myapplication.view.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -27,6 +30,17 @@ class MainFragment : Fragment() {
         FragmentMainBinding.inflate(inflater, container, false)
         _binding = FragmentMainBinding.inflate(inflater, container, false)
 
+        _binding.searchBar.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("Mykyta","${s}")
+            }
+        })
         return _binding.root
     }
 
